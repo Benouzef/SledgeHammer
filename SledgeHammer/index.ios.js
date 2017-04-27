2019bdb07375fb31cc11253d44a20e4f27b561f5
Benouzef <benoit.fillon@gmail.com>
2017-04-27 18:24:07 +0200

    First changes

--- SledgeHammer/index.ios.js
+++ SledgeHammer/index.ios.js
@@ -4,50 +4,19 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+ import React, { Component } from 'react';
+ import {
+   AppRegistry,
+ } from 'react-native';
 
-export default class SledgeHammer extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+ import MainActivity from './mainactivity';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+ export default class SledgeHammer extends Component {
+   render() {
+     return (
+       <MainActivity />
+     );
+   }
+ }
 
-AppRegistry.registerComponent('SledgeHammer', () => SledgeHammer);
+ AppRegistry.registerComponent('SledgeHammer', () => SledgeHammer);
