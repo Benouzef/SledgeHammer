4c3a7374eb850a055329f71ed05c1ff5876d7270
Benouzef <benoit.fillon@gmail.com>
2017-07-06 13:45:12 +0200

    Upgrade to RN 0.46 and removal of native-base

--- baretoolsapp/src/screens/ProfileScreen.js
+++ baretoolsapp/src/screens/ProfileScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Image, Text, Button, View, TouchableOpacity } from 'react-native';
-import { Form, Item, Label, Input } from 'native-base';
+//import { Form, Item, Label, Input } from 'native-base';
 import { signInWithGoogleAsync, getSignatureStamp, setApiToken, getIndeptiveFolder } from '../utilities/GoogleDrive';
 import GoogleSignIn from 'react-native-google-sign-in';
 import * as firebase from 'firebase';
@@ -56,70 +56,8 @@ export default class ProfileScreen extends React.Component {
     return (
       <ScrollView>
         <Text>PERSONAL INFORMATION</Text>
-        <Form>
-          <Item floatingLabel>
-            <Label>Full Name</Label>
-            <Input value={user.name}/>
-          </Item>
-          <Item floatingLabel>
-            <Label>Email</Label>
-            <Input />
-          </Item>
-          <Item floatingLabel last>
-            <Label>Phone</Label>
-            <Input />
-          </Item>
-        </Form>
         <Text>COMPANY INFORMATION</Text>
-        <Form>
-          <Item floatingLabel>
-            <Label>Company Name</Label>
-            <Input />
-          </Item>
-          <Item floatingLabel>
-            <Label>Address Line #1</Label>
-            <Input />
-          </Item>
-          <Item floatingLabel>
-            <Label>Address Line #2</Label>
-            <Input />
-          </Item>
-          <Item floatingLabel>
-            <Label>Company nature</Label>
-            <Input />
-          </Item>
-          <Item floatingLabel>
-            <Label>Company Registration office</Label>
-            <Input />
-          </Item>
-          <Item floatingLabel>
-            <Label>Company Reg. #</Label>
-            <Input />
-          </Item>
-          <Item floatingLabel>
-            <Label>Company Code</Label>
-            <Input />
-          </Item>
-          <Item floatingLabel last>
-            <Label>VAT # (if applicable)</Label>
-            <Input />
-          </Item>
-        </Form>
         <Text>BANK INFORMATION</Text>
-        <Form>
-          <Item floatingLabel>
-            <Label>Bank</Label>
-            <Input />
-          </Item>
-          <Item floatingLabel>
-            <Label>IBAN</Label>
-            <Input />
-          </Item>
-          <Item floatingLabel last>
-            <Label>BIC</Label>
-            <Input />
-          </Item>
-        </Form>
         <Text>SIGNATURE STAMP</Text>
         <TouchableOpacity
           onPress={
