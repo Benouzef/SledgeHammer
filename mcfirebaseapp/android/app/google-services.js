b43d1a14aa482d4e1b506cb25fc6a20af38088b7
Benouzef <benoit.fillon@gmail.com>
2017-10-07 16:30:24 +0200

    Initial setup

---
+++ mcfirebaseapp/android/app/google-services.js
@@ -0,0 +1,42 @@
+{
+  "project_info": {
+    "project_number": "45295109591",
+    "firebase_url": "https://skillvaluemasterclass.firebaseio.com",
+    "project_id": "skillvaluemasterclass",
+    "storage_bucket": "skillvaluemasterclass.appspot.com"
+  },
+  "client": [
+    {
+      "client_info": {
+        "mobilesdk_app_id": "1:45295109591:android:c116ff49ae7e40b0",
+        "android_client_info": {
+          "package_name": "com.mcfirebaseapp"
+        }
+      },
+      "oauth_client": [
+        {
+          "client_id": "45295109591-nt5ob968mg0vkpsjcee4kf55msslgv7k.apps.googleusercontent.com",
+          "client_type": 3
+        }
+      ],
+      "api_key": [
+        {
+          "current_key": "AIzaSyD6CQW8nUxUJ5g5c7PXmwyWGWPEQXL5EBA"
+        }
+      ],
+      "services": {
+        "analytics_service": {
+          "status": 1
+        },
+        "appinvite_service": {
+          "status": 1,
+          "other_platform_oauth_client": []
+        },
+        "ads_service": {
+          "status": 2
+        }
+      }
+    }
+  ],
+  "configuration_version": "1"
+}
