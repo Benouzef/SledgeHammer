6d053db0a301517a584195e94d489792ece5d1b6
benouzef <benoit.fillon@gmail.com>
2018-11-12 16:23:22 +0100

    Changes to support Linux Ubuntu

--- Node101MasterClass/02_APIsWithExpress/mock-api/endpoints.js
+++ Node101MasterClass/02_APIsWithExpress/mock-api/endpoints.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 const router = require('express').Router();
 
-router.get('/customers/', file('mock-api/Customers.json'));
-router.get('/customers/:id/', file('mock-api/Customer.json'));
+router.get('/customers/', file('./Node101MasterClass/02_APIsWithExpress/mock-api/Customers.json'));
+router.get('/customers/:id/', file('./Node101MasterClass/02_APIsWithExpress/mock-api/Customer.json'));
 
 function file(filename) {
   return (request, response) => {
     response.writeHead(200, 'OK', {'Content-Type': 'application/json'});
+    console.log(process.cwd())
     fs.createReadStream(filename).pipe(response);
   };
 }
